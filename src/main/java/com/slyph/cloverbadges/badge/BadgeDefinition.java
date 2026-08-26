90e3b65cc4a9a416488dc096331f7d5ed2a49b6e
./slyph <118862741+slyphmp4@users.noreply.github.com>
2026-08-26 08:48:59 +0300

    Add badge priority

--- src/main/java/com/slyph/cloverbadges/badge/BadgeDefinition.java
+++ src/main/java/com/slyph/cloverbadges/badge/BadgeDefinition.java
@@ -1,6 +1,6 @@
 package com.slyph.cloverbadges.badge;
 
-public record BadgeDefinition(String id, String name, String text, String permission) {
+public record BadgeDefinition(String id, String name, String text, String permission, int priority) {
     public boolean hasPermissionSource() {
         return permission != null && !permission.isBlank();
     }
