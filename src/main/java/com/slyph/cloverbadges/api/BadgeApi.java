b547f8ab327d081147d121dc0308f8f4bb0d6431
./slyph <118862741+slyphmp4@users.noreply.github.com>
2026-08-26 08:49:38 +0300

    Expose multiple active badges in API

--- src/main/java/com/slyph/cloverbadges/api/BadgeApi.java
+++ src/main/java/com/slyph/cloverbadges/api/BadgeApi.java
@@ -3,10 +3,13 @@ package com.slyph.cloverbadges.api;
 import net.kyori.adventure.text.Component;
 import org.bukkit.OfflinePlayer;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public interface BadgeApi {
+    List<String> getActiveBadgeIds(OfflinePlayer player);
+
     Optional<String> getActiveBadgeId(OfflinePlayer player);
 
     Component getActiveBadgeComponent(OfflinePlayer player);
