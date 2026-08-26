c665f7a4d44d81cad2c8d649c4b2b082e8ead1a6
./slyph <118862741+slyphmp4@users.noreply.github.com>
2026-08-26 08:00:00 +0300

    Add plugin bootstrap

---
+++ src/main/java/com/slyph/cloverbadges/CloverBadges.java
@@ -0,0 +1,83 @@
+package com.slyph.cloverbadges;
+
+import com.slyph.cloverbadges.api.BadgeApi;
+import com.slyph.cloverbadges.badge.BadgeRegistry;
+import com.slyph.cloverbadges.command.BadgeCommand;
+import com.slyph.cloverbadges.command.BadgeTabCompleter;
+import com.slyph.cloverbadges.config.ConfigManager;
+import com.slyph.cloverbadges.listener.PlayerListener;
+import com.slyph.cloverbadges.message.MessageService;
+import com.slyph.cloverbadges.placeholder.CloverBadgesExpansion;
+import com.slyph.cloverbadges.player.PlayerBadgeService;
+import com.slyph.cloverbadges.storage.PlayerDataStore;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.Objects;
+
+public final class CloverBadges extends JavaPlugin {
+    private ConfigManager configManager;
+    private BadgeRegistry badgeRegistry;
+    private MessageService messageService;
+    private PlayerBadgeService badgeService;
+    private CloverBadgesExpansion expansion;
+    private BukkitTask cleanupTask;
+
+    @Override
+    public void onEnable() {
+        configManager = new ConfigManager(this);
+        badgeRegistry = new BadgeRegistry(configManager);
+        messageService = new MessageService(configManager);
+        PlayerDataStore dataStore = new PlayerDataStore(this);
+        badgeService = new PlayerBadgeService(this, badgeRegistry, dataStore);
+
+        PluginCommand badgeCommand = Objects.requireNonNull(getCommand("badge"));
+        badgeCommand.setExecutor(new BadgeCommand(this, badgeService, messageService));
+        badgeCommand.setTabCompleter(new BadgeTabCompleter(badgeService));
+
+        getServer().getPluginManager().registerEvents(new PlayerListener(badgeService), this);
+        getServer().getServicesManager().register(BadgeApi.class, badgeService, this, ServicePriority.Normal);
+
+        if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            expansion = new CloverBadgesExpansion(this, badgeService);
+            expansion.register();
+        }
+
+        scheduleCleanup();
+        getLogger().info("CloverBadges v" + getPluginMeta().getVersion() + " enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        if (cleanupTask != null) {
+            cleanupTask.cancel();
+        }
+        if (expansion != null) {
+            expansion.unregister();
+        }
+        if (badgeService != null) {
+            badgeService.saveAll();
+        }
+        getServer().getServicesManager().unregisterAll(this);
+    }
+
+    public void reloadPlugin() {
+        configManager.reload();
+        badgeRegistry.reload();
+        scheduleCleanup();
+    }
+
+    private void scheduleCleanup() {
+        if (cleanupTask != null) {
+            cleanupTask.cancel();
+        }
+        long interval = Math.max(20L, getConfig().getLong("storage.cleanup-interval-ticks", 6000L));
+        cleanupTask = getServer().getScheduler().runTaskTimer(this, badgeService::cleanupExpired, interval, interval);
+    }
+
+    public BadgeApi getBadgeApi() {
+        return badgeService;
+    }
+}
