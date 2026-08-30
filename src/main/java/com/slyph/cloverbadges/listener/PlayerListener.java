e9001f05ca3151c494e94674b7b1ea7bf2c9041e
./slyph <118862741+slyphmp4@users.noreply.github.com>
2026-08-30 13:10:52 +0300

    Initialize starter nickname color on join

--- src/main/java/com/slyph/cloverbadges/listener/PlayerListener.java
+++ src/main/java/com/slyph/cloverbadges/listener/PlayerListener.java
@@ -1,5 +1,6 @@
 package com.slyph.cloverbadges.listener;
 
+import com.slyph.cloverbadges.nicknamecolor.PlayerNicknameColorService;
 import com.slyph.cloverbadges.player.PlayerBadgeService;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,19 +8,23 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class PlayerListener implements Listener {
-    private final PlayerBadgeService service;
+    private final PlayerBadgeService badgeService;
+    private final PlayerNicknameColorService paintService;
 
-    public PlayerListener(PlayerBadgeService service) {
-        this.service = service;
+    public PlayerListener(PlayerBadgeService badgeService, PlayerNicknameColorService paintService) {
+        this.badgeService = badgeService;
+        this.paintService = paintService;
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        service.ensure(event.getPlayer());
+        badgeService.ensure(event.getPlayer());
+        paintService.ensure(event.getPlayer());
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        service.saveAll();
+        badgeService.saveAll();
+        paintService.saveAll();
     }
 }
